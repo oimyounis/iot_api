8424aacd3c43084c9d26a7b420793101d2abb722
Omar Younis <thehaker_2005@hotmail.com>
2019-02-15 14:54:07 +0300

    update

--- api/webhooks.py
+++ api/webhooks.py
@@ -15,16 +15,11 @@ def index(request):
     device_id = data['client_id']
 
     if data['action'] == 'client_connected':
-        device = Device.objects\
+        Device.objects\
             .on_conflict(['id'], ConflictAction.UPDATE)\
             .insert_and_get(id=device_id,
                             online=True)
 
-        device.temps.create(value=29)
-        device.temps.create(value=35)
-
-        device.bps.create(value=520)
-        device.bps.create(value=580)
     elif data['action'] == 'client_disconnected':
         Device.objects\
             .on_conflict(['id'], ConflictAction.UPDATE)\
